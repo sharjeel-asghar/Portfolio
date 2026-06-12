98b2dee8092da8f673f3bf4a5481f6c3c3aa9404
shary665 <retro786gaming665@gmail.com>
2026-06-12 22:45:02 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./components/NavBar"
+import Hero from "./sections/Hero"
+import LogoShowcase from "./sections/LogoShowcase"
+import AppShowcase from "./sections/ShowcaseSection"
+
+const App = () => {
+  return (
+    <>
+    <NavBar />
+    <Hero />
+    <AppShowcase />
+    <LogoShowcase />
+    </>
+  )
+}
+
+export default App
